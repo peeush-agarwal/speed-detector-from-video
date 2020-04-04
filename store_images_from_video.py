01b92a34fc19baab61c35215725d60b202183d91
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-04-04 09:51:55 +0530

    move main to main.py

--- store_images_from_video.py
+++ store_images_from_video.py
@@ -17,11 +17,3 @@ def store_images_from_video(video_file_name, output_folder):
     
     cap.release()
     cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    input_folder = './Data/train'
-    output_folder = './Data/output'
-    video_filename = 'train.mp4'
-    text_filename = 'train.txt'
-    store_images_from_video(os.path.join(input_folder, video_filename), output_folder)
-    print('Images fetched from video:'+video_filename)
